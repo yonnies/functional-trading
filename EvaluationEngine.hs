0e71b2146728d9fc34d6b4970df8ece1e943918b
yonnies <yoni.yakova@gmail.com>
2025-02-26 11:01:59 +0000

    removed type checking from the evaluation engine

--- EvaluationEngine.hs
+++ EvaluationEngine.hs
@@ -77,73 +77,90 @@ optimiseContract (One cur) d
 
 ----------------------------- Evaluation -----------------------------
 
-type EvalM a = ExceptT String (State Cache) a
+----------------------------------------------------------------------------
+-- 1. Top-level eval_contract
+----------------------------------------------------------------------------
+
+type EvalM a = (State Cache) a
 type Cache = Map.Map Contract (PR Double)
 
 eval_contract :: Model -> Contract -> Date -> Either String (PR Double)
-eval_contract model contract date = case optimiseContract contract date of
+eval_contract model c d = case optimiseContract c d of
     Left err -> Left err
-    Right optimisedContract -> evalState (runExceptT (eval_contract_cache model optimisedContract date)) Map.empty
+    Right c' -> Right (evalState (evalC model c') Map.empty)
 
-eval_contract_cache :: Model -> Contract -> Date -> EvalM (PR Double)
-eval_contract_cache model@(Model startDate stepSize constPr discount snell exchange _) contract date = do
+----------------------------------------------------------------------------
+-- 2. Memoized evaluation of a contract
+----------------------------------------------------------------------------
+
+evalC :: Model -> Contract -> EvalM (PR Double)
+evalC model@(Model _ _ constPr discount snell exchange _) contract = do
   cache <- get
   case Map.lookup contract cache of
+    -- If this contract is already in the cache, just return it:
     Just result -> return result
+    -- Otherwise evaluate and store it:
     Nothing -> do
-      result <- eval contract date
+      result <- eval contract
       modify (Map.insert contract result)
       return result
   where
-    eval :: Contract -> Date -> EvalM (PR Double)
-    eval None d 
-      | d == infiniteHorizon = throwError "Error: Contract has no acquisition date set."
-      | otherwise            = return (constPr 0)
-    
-    eval (One cur) d 
-      | d == infiniteHorizon = throwError "Error: Contract has no acquisition date set."
-      | otherwise            = return (exchange cur)
-    
-    eval (Give c) d = do
-      pr <- eval_contract_cache model c d
-      return (negate pr)
-    
-    eval (And c1 c2) d = do
-      pr1 <- eval_contract_cache model c1 d
-      pr2 <- eval_contract_cache model c2 d
-      return (pr1 + pr2)
-    
-    eval (Or c1 c2) d = do
-      pr1 <- eval_contract_cache model c1 d
-      pr2 <- eval_contract_cache model c2 d
-      return (max pr1 pr2)
-    
-    eval (AcquireOn d2 c) d1
-      | d1 /= infiniteHorizon, d2 < d1 = throwError "Error: Attempting to acquire contract with an earlier expiry date at a later date."
-      | otherwise = do
-          val <- eval_contract_cache model c d2
-          return (discount d2 val)
-    
-    eval (AcquireOnBefore d2 c) d1
-      | d1 /= infiniteHorizon, d2 < d1 = throwError "Error: Attempting to acquire contract with an earlier expiry date at a later date."
-      | otherwise = do
-          val <- eval_contract_cache model c d2
-          return (snell d2 val)
-    
-    eval (Scale obs c) d = do
-      val    <- eval_contract_cache model c d
-      obsVal <- eval_obs model obs
-      return (obsVal * val)
+    ----------------------------------------------------------------------------
+    -- 2a. The local `eval` function does the real recursive work
+    ----------------------------------------------------------------------------
+    eval :: Contract -> EvalM (PR Double)
+    eval None =
+      return (constPr 0)
 
-eval_obs :: Model -> Obs Double -> EvalM (PR Double)
-eval_obs model@(Model _ _ constPr _ _ _ stockModel) obs = do
+    eval (One cur) =
+      return (exchange cur)
+
+    eval (Give c) = do
+      cVal <- evalC model c
+      return (negate cVal)
+
+    eval (And c1 c2) = do
+      v1 <- evalC model c1
+      v2 <- evalC model c2
+      return (v1 + v2)
+
+    eval (Or c1 c2) = do
+      v1 <- evalC model c1
+      v2 <- evalC model c2
+      return (max v1 v2)
+
+    eval (AcquireOn d c) = do
+      cVal <- evalC model c
+      return (discount d cVal)
+
+    eval (AcquireOnBefore d c) = do
+      cVal <- evalC model c
+      return (snell d cVal)
+
+    eval (Scale obs c) = do
+      obsVal <- evalO model obs
+      cVal   <- evalC model c
+      return (obsVal * cVal)
+
+----------------------------------------------------------------------------
+-- 3. Evaluating an observation with memoization
+----------------------------------------------------------------------------
+
+evalO :: Model -> Obs Double -> EvalM (PR Double)
+evalO model@(Model _ _ constPr _ _ _ stockModel) obs = do
+  -- We reuse the same contract->PR cache, so we need a "fake" contract
+  -- that represents “Scale obs (one GBP)” as a key.  This is a bit hacky but works
+  -- because it would give correct result even if there is a colision.
+  let key = Scale obs (one GBP)
+
   cache <- get
-  let key = Scale obs None
   case Map.lookup key cache of
     Just result -> return result
     Nothing -> do
-      result <- case obs of
-                  Konst k       -> return (constPr k)
-                  StockPrice stk -> return (stockModel stk)
+      result <- eval obs
       modify (Map.insert key result)
       return result
+      where 
+        eval :: Obs Double -> EvalM (PR Double)
+        eval (Konst k) = return (constPr k)
+        eval (StockPrice stk) = return (stockModel stk)
